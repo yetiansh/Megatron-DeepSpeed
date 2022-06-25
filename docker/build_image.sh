f0702156f05765d62fcfc8ade9dbf495acdc08fb
yetiansh <yetiansh@gmail.com>
2022-06-25 21:56:03 +0800

    update

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-nvidia-docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-DOCKER_BUILDKIT=1 nvidia-docker buildx build -f Dockerfile -t megatron_ds:exp .
+DOCKER_BUILDKIT=1 nvidia-docker build -f Dockerfile -t megatron_ds:exp .
