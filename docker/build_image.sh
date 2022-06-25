a2aa730c202d6e44faca7eb6951b1b6fa9a32b13
yetiansh <yetiansh@gmail.com>
2022-06-25 09:59:18 +0800

    fix

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 nvidia-docker build -f Dockerfile -t megatron_ds:exp .
+nvidia-docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
+DOCKER_BUILDKIT=1 nvidia-docker buildx build -f Dockerfile -t megatron_ds:exp .
--- ds_download_ckpt.sh
+++ ds_download_ckpt.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+mkdir -p /Megatron-DeepSpeed/data
 
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
